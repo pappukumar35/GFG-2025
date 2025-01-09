241cd4118285dd61d607c56e9008994197213a03
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2025-01-09 20:32:30 +0530

    Add files via upload

---
+++ January/SubArray_Sum.java
@@ -0,0 +1,35 @@
+package January;
+
+import java.util.*;
+
+public class SubArray_Sum {
+     static ArrayList<Integer> subarraySum(int[] arr, int target) {
+          int n = arr.length;
+          int currSum = 0;
+          int start = 0;
+          int end = 0;
+          ArrayList<Integer> res = new ArrayList<>();
+          while (end < n) {
+               if (currSum < target) {
+                    currSum += arr[end++];
+               }
+               if (currSum == target) {
+                    res.add(start + 1);
+                    res.add(end);
+                    break;
+               }
+               if (currSum > target) {
+                    currSum = 0;
+                    start++;
+                    end = start;
+               }
+          }
+          if (res.isEmpty())
+               res.add(-1);
+          return res;
+     }
+
+     public static void main(String[] args) {
+
+     }
+}
