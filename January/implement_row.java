cb7d0ec4d0cb9e4c5481b5dcb7a7b820b66a49b7
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2025-01-29 20:58:22 +0530

    Add files via upload

---
+++ January/implement_row.java
@@ -0,0 +1,13 @@
+package January;
+
+import java.util.*;
+
+public class implement_row {
+     double power(double b, int e) {
+          return Math.pow(b, e);
+     }
+
+     public static void main(String[] args) {
+
+     }
+}
