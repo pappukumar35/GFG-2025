46d5735947c8db514852dacd68995fe3cb7e045e
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2025-03-15 20:24:41 +0530

    Add files via upload

---
+++ March/coin2.java
@@ -0,0 +1,22 @@
+package March;
+
+public class coin2 {
+     public int minCoins(int coins[], int sum) {
+          // code here
+          int[] dp = new int[sum + 1];
+          Arrays.fill(dp, sum + 1);
+          int n = coins.length;
+          dp[0] = 0;
+          for (int num : coins) {
+               for (int j = num; j <= sum; j++) {
+                    dp[j] = Math.min(dp[j], 1 + dp[j - num]);
+               }
+          }
+
+          return dp[sum] == sum + 1 ? -1 : dp[sum];
+     }
+
+     public static void main(String[] args) {
+
+     }
+}
