da7cf75b48dc3269e053a85d3c7e306c85764d7c
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2025-03-21 13:51:26 +0530

    Add files via upload

---
+++ March/teack.java
@@ -0,0 +1,25 @@
+package March;
+
+public class teack {
+     int f(int ind, int arr[], int dp[]) {
+          if (ind < 0)
+               return 0;
+
+          if (dp[ind] != -1)
+               return dp[ind];
+          int nt = f(ind - 1, arr, dp);
+          int t = arr[ind] + f(ind - 2, arr, dp);
+          return dp[ind] = Math.max(nt, t);
+     }
+
+     public int findMaxSum(int arr[]) {
+          int dp[] = new int[arr.length];
+          Arrays.fill(dp, -1);
+          return f(arr.length - 1, arr, dp);
+
+     }
+
+     public static void main(String[] args) {
+
+     }
+}
