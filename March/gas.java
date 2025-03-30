7b4f388b2690a483d2b3170e5472cdfdf127579c
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2025-03-30 13:20:41 +0530

    Add files via upload

---
+++ March/gas.java
@@ -0,0 +1,28 @@
+package March;
+
+public class gas {
+     public int startStation(int[] gas, int[] cost) {
+          int n = gas.length;
+
+          int sum = 0;
+          int start = 0;
+          int total = 0;
+
+          for (int i = 0; i < 2 * n; i++) {
+               int index = i % n;
+               sum += gas[index] - cost[index];
+               total += gas[index] - cost[index];
+
+               if (sum < 0) {
+                    sum = 0;
+                    start = i + 1;
+               }
+          }
+
+          return total >= 0 ? start % n : -1;
+     }
+
+     public static void main(String[] args) {
+
+     }
+}
