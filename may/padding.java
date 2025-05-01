ee450f12959e7daa4e780a3fb691011905b81de7
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2025-05-01 19:57:30 +0530

    Add files via upload

---
+++ may/padding.java
@@ -0,0 +1,19 @@
+package may;
+
+public class padding {
+     ArrayList<Integer> nthRowOfPascalTriangle(int n) {
+          ArrayList<Integer> list = new ArrayList<>();
+          list.add(1);
+          int ans = 1;
+          for (int i = 1; i < n; i++) {
+               ans = ans * (n - i);
+               ans = ans / i;
+               list.add(ans);
+          }
+          return list;
+     }
+
+     public static void main(String[] args) {
+
+     }
+}
