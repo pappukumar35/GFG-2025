cd6d3956260d572d129c7c0b639d796b05d9a8fd
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2025-05-06 14:35:23 +0530

    Add files via upload

---
+++ may/leftview.java
@@ -0,0 +1,25 @@
+package may;
+
+import java.util.*;
+
+public class leftview {
+     void f(Node node, ArrayList<Integer> arr, int lvl) {
+          if (node == null)
+               return;
+          if (arr.size() < lvl)
+               arr.add(node.data);
+
+          f(node.left, arr, lvl + 1);
+          f(node.right, arr, lvl + 1);
+     }
+
+     ArrayList<Integer> leftView(Node root) {
+          ArrayList<Integer> ans = new ArrayList<>();
+          f(root, ans, 1);
+          return ans;
+     }
+
+     public static void main(String[] args) {
+
+     }
+}
