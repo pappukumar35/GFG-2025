3429851c3d562d27b0794906b7d18aa8543771e3
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2025-05-30 23:51:56 +0530

    Add files via upload

---
+++ may/clost.java
@@ -0,0 +1,29 @@
+package may;
+
+public class clost {
+     public int findMaxFork(Node root, int k) {
+          // code here.
+          if (root == null)
+               return -1;
+          if (root.data == k)
+               return k;
+          int left = -1;
+          if (root.left != null && root.data > k) {
+               left = findMaxFork(root.left, k);
+          }
+          int right = -1;
+          if (root.right != null && root.data < k) {
+               right = findMaxFork(root.right, k);
+          }
+
+          if (root.data < k) {
+               return Math.max(root.data, Math.max(left, right));
+          } else {
+               return Math.max(left, right);
+          }
+     }
+
+     public static void main(String[] args) {
+
+     }
+}
